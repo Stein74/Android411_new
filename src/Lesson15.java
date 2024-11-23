aee1561d6ea05862daea9e15816461f342c394d7
Stein74 <pstr@list.ru>
2024-11-23 13:50:49 +0300

    lesson15_

--- src/Lesson15.java
+++ src/Lesson15.java
@@ -6,11 +6,11 @@ public class Lesson15 {
 
     public static void main(String[] args) {
 
-        String str = "Hello, World!";
+        String str = "Hello, _World!";
 
         System.out.printf("Дана строка - \"%s\"\n", str);
         System.out.printf("Количество строчных букв - %d, количество прописных букв - %d",
-                    str.replaceAll("[A-Z\\d\\W]", "").length(),
-                    str.replaceAll("[a-z\\d\\W]", "").length());
+                    str.replaceAll("[A-Z_\\d\\W]", "").length(),
+                    str.replaceAll("[a-z_\\d\\W]", "").length());
     }
 }
