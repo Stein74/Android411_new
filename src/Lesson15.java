a690d52f7cdd0cd0cc837a5172d0dc9c5b3d2ded
Stein74 <pstr@list.ru>
2024-11-23 13:52:35 +0300

    lesson15__

--- src/Lesson15.java
+++ src/Lesson15.java
@@ -6,7 +6,7 @@ public class Lesson15 {
 
     public static void main(String[] args) {
 
-        String str = "Hello, _World!";
+        String str = "Hello, World! _ Привет, мир!";
 
         System.out.printf("Дана строка - \"%s\"\n", str);
         System.out.printf("Количество строчных букв - %d, количество прописных букв - %d",
